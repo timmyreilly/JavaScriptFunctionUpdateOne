2ca26212bb4531958fed4de8c8155ca532b2852d
timmyreilly <timreilly@live.com>
2018-10-16 13:38:19 -0700

    decent updating

--- AddToArrayInDocumentById/index.js
+++ AddToArrayInDocumentById/index.js
@@ -11,7 +11,7 @@ module.exports = async function (context, req) {
     // If there are no documents in the DB with that ID we need to create a Document. 
 
 
-    if (req.body.id) {
+    if (req.body.id && req.body.eventType) {
 
         const { container, database } = await cosmos.init("sample database", "sample collection"); 
 
@@ -25,22 +25,28 @@ module.exports = async function (context, req) {
             ]
         };
 
-        const { result: results } = await client.database("sample database").container("sample collection").items.query(querySpec).toArray();
-        const { result: r } = await container.items.query(querySpec).toArray(); 
+        // Get the current entry from the database:
+        const { result: results } = await container.items.query(querySpec).toArray(); 
 
+        // create the event to append to events array in document: 
+        const newEvent = { eventType : req.body.eventType, ts : req.body.ts }; 
 
-        var newEvent = { eventType : req.body.eventType, ts : req.body.ts }; 
+        // create an empty document to upsert if no id is found in collection: 
+        let blankEntry = { id : req.body.id.toString(), "events" : [] };
 
-        var newDoc = (results[0]); 
-        newDoc.content.events.push(newEvent); 
+        // get the new document in place - either the result or a blank entry
+        let newDoc = results[0] || blankEntry; 
 
+        // push our event from the request into the 'newish' document 
+        newDoc.events.push(newEvent); 
+
+        // upsert our document! 
         const { body } = await container.items.upsert(newDoc);
-
 
 
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.id || req.body.id) + " " + JSON.stringify(results) + " " + JSON.stringify(body) 
+            body: (req.query.id || req.body.id) + " was found... \nThis \n " + JSON.stringify(results) + " \n is now this: \n " + JSON.stringify(body) 
 
         };
         
@@ -48,7 +54,7 @@ module.exports = async function (context, req) {
     else {
         context.res = {
             status: 400,
-            body: "Please pass a id on the query string or in the request body"
+            body: "Please pass an id on the request body"
         };
     }
 };
